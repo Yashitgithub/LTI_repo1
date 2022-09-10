84746412d65f9a1ef64bab60d4e1f0449999570d
Yashitgithub <ysssit91@gmail.com>
2022-09-10 17:22:58 +0530

    Committed!

---
+++ DemoMaven_Project/src/test/java/com/lti/MavenDemo/TestDemo.java
@@ -0,0 +1,17 @@
+package com.lti.MavenDemo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestDemo {
+	@Test
+	void strcheck() {
+		// TODO Auto-generated method stub
+		String s1 = "demo";
+		System.out.println("This is testcase for string check");
+		assertEquals("demo",s1);
+		
+	}
+	
+}
